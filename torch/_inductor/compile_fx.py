e4e4d7044716b30296599aac10d0c9458475dfca
leslie-fang-intel <leslie.fang@intel.com>
2023-03-17 12:57:29 +0800

    Enable lowering to inductor
    ghstack-source-id: db9799ee8d8d63324103b91cbda0b9c62ec312ef
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/96927

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -445,10 +445,6 @@ def compile_fx(
         )
 
     if isinstance(model_, torch.fx.GraphModule):
-        with overrides.patch_functions():
-            model_ = overrides.replace_fx(model_)
-            model_ = overrides.fuse_fx(model_, example_inputs_)
-
         if isinstance(model_.graph._codegen, _PyTreeCodeGen):
             # this graph is the result of dynamo.export()
             return handle_dynamo_export_graph(
@@ -456,6 +452,10 @@ def compile_fx(
                 example_inputs_,
                 recursive_compile_fx,
             )
+
+        with overrides.patch_functions():
+            model_ = overrides.replace_fx(model_)
+            model_ = overrides.fuse_fx(model_, example_inputs_)
 
     if any(isinstance(x, (list, tuple, dict)) for x in example_inputs_):
         return flatten_graph_inputs(
